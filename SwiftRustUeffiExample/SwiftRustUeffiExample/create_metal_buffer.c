19b54dd9bd8c7a4546cc9db5a4bd980d19b78209
Dmitry Ivanov <idmitry90@gmail.com>
2024-09-07 15:26:53 +0100

    Succesfully allocate 256 MB, which we don't see (only when profiling)

---
+++ SwiftRustUeffiExample/SwiftRustUeffiExample/create_metal_buffer.c
@@ -0,0 +1,8 @@
+//
+//  create_metal_buffer.c
+//  SwiftRustUeffiExample
+//
+//  Created by g00dm0us3 on 9/7/24.
+//
+
+#include "create_metal_buffer.h"
---
+++ SwiftRustUeffiExample/SwiftRustUeffiExample/create_metal_buffer.h
@@ -0,0 +1,13 @@
+//
+//  create_metal_buffer.h
+//  SwiftRustUeffiExample
+//
+//  Created by g00dm0us3 on 9/7/24.
+//
+
+#ifndef create_metal_buffer_h
+#define create_metal_buffer_h
+
+#include <stdio.h>
+
+#endif /* create_metal_buffer_h */
